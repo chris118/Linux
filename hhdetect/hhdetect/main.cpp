f4979b63624d5b7f4a034bf05bcf1fac85c1aeca
xiaopeng <12405301@qq.com>
2016-11-21 09:20:18 +0800

    add

---
+++ hhdetect/hhdetect/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hhdetect
+//
+//  Created by xiaopeng on 2016/11/16.
+//  Copyright © 2016年 putao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
